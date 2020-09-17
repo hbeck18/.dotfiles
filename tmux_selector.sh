eae7d0851fc69790a326f0166ee6ecbfa1e5fc89
hbeck18 <heiner_beckmeyer@live.de>
2020-09-17 10:10:48 +0200

    2020-09-17 09:44:14

--- tmux_selector.sh
+++ tmux_selector.sh
@@ -2,8 +2,8 @@
 sudo mount -t drvfs Z: /mnt/z
 if [[ "$OSTYPE" == "darwin"*  ]]; then
     # gamma exposure
-    cd "$HOME/Google Drive/__Promotion/__gamma_exposure/02_programs/"
-    tmuxp load "$HOME/Google Drive/__Promotion/__gamma_exposure/02_programs/_tmux_session.yaml" -d
+    cd "$HOME/Google Drive/_Promotion/__gamma_exposure/02_programs/"
+    tmuxp load "$HOME/Google Drive/_Promotion/__gamma_exposure/02_programs/_tmux_session.yaml" -d
     # Fed tails
     cd "$HOME/sciebo/__active/fed_call/Programs/__Python/"
     tmuxp load "$HOME/sciebo/__active/fed_call/Programs/__Python/_tmux_session.yaml" -d
@@ -12,8 +12,8 @@ if [[ "$OSTYPE" == "darwin"*  ]]; then
 else
     if grep -q Microsoft /proc/version; then
         # gamma exposure
-        cd "/mnt/c/Users/h_beck18/Google Drive/__Promotion/__gamma_exposure/02_programs/"
-        tmuxp load "/mnt/c/Users/h_beck18/Google Drive/__Promotion/__gamma_exposure/02_programs/_tmux_session.yaml" -d
+        cd "/mnt/c/Users/h_beck18/Google Drive/_Promotion/__gamma_exposure/02_programs/"
+        tmuxp load "/mnt/c/Users/h_beck18/Google Drive/_Promotion/__gamma_exposure/02_programs/_tmux_session.yaml" -d
         # Fed tails
         cd "/mnt/c/Users/h_beck18/sciebo/__active/fed_call/Programs/__Python/"
         tmuxp load "/mnt/c/Users/h_beck18/sciebo/__active/fed_call/Programs/__Python/_tmux_session.yaml" -d
@@ -21,8 +21,8 @@ else
         cd "/mnt/z/_projects/network_estimation/02_programs/"
         tmuxp load "/mnt/z/_projects/network_estimation/02_programs/_tmux_session.yaml" -d
         # inventory
-        cd "/mnt/c/Users/h_beck18/Google Drive/__Promotion/__inventory_data/02_programs/"
-        tmuxp load "/mnt/c/Users/h_beck18/Google Drive/__Promotion/__inventory_data/02_programs/_tmux_session.yaml" -d
+        cd "/mnt/c/Users/h_beck18/Google Drive/_Promotion/__inventory_data/02_programs/"
+        tmuxp load "/mnt/c/Users/h_beck18/Google Drive/_Promotion/__inventory_data/02_programs/_tmux_session.yaml" -d
         # corona fx networks
         cd "/mnt/c/Users/h_beck18/sciebo/__active/corona_network/02_programs"
         tmuxp load "/mnt/c/Users/h_beck18/sciebo/__active/corona_network/02_programs/_tmux_session.yaml" -d
